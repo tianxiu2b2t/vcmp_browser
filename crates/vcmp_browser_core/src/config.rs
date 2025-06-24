3955052120a3ed3e4c5d5ea78a837bdba960ff35
tianxiu2b2t <atianxiua@163.com>
2025-06-24 23:59:55 +0800

    refactor: 重构配置

--- crates/vcmp_browser_core/src/config.rs
+++ crates/vcmp_browser_core/src/config.rs
@@ -1,4 +1,4 @@
-use std::{default::Default, path::PathBuf, sync::OnceLock};
+/*use std::{default::Default, path::PathBuf, sync::OnceLock};
 
 use tracing::{Level, event};
 
@@ -30,16 +30,17 @@ impl Default for Config {
     }
 }
 
-impl Into<toml::Value> for Config {
-    fn into(self) -> toml::Value {
-        let mut table = toml::value::Table::new();
+impl Into<toml::Table> for Config {
+    fn into(self) -> toml::Table {
+        let mut table = toml::Table::new();
         table.insert("index_url".to_string(), self.index_url.into());
-        toml::Value::Table(table)
+        toml::Value(table).into()
     }
 }
 
 impl Config {
     fn to_pretty_string() -> String {
+        use toml::to_string_pretty;
         format!(
             r#"# vcmp_browser config
 # This is a config file for vcmp_browser.
@@ -47,7 +48,7 @@ impl Config {
 # If you don't know what you are doing, please don't edit this file.
 
 {}"#,
-            toml::to_string_pretty(&CONFIG.get().unwrap().into()).unwrap()
+            to_string_pretty::<toml::Table>(get_config().into()).unwrap()
         )
     }
 
@@ -56,7 +57,7 @@ impl Config {
         if !path.exists() {
             event!(Level::INFO, "Not found config. Use default config.");
             return Self::default();
-        } 
+        }
         let config_content = std::fs::read_to_string(path).unwrap();
         Self::from(toml::from_str::<toml::Table>(&config_content).unwrap())
     }
@@ -71,9 +72,13 @@ impl Config {
 
 pub static CONFIG: OnceLock<Config> = OnceLock::new();
 
+pub fn get_config_file() -> PathBuf {
+    get_home_dir().join("config.toml")
+}
+
 pub fn init() {
     let config = &CONFIG;
-    let config_path = get_home_dir().join("config.toml");
+    let config_path = get_config_file();
     config.set(Config::load_from_path(config_path.clone())).unwrap();
 
     get_config().save_to_path(config_path.clone());
@@ -81,4 +86,60 @@ pub fn init() {
 
 pub fn get_config() -> Config {
     CONFIG.get().unwrap().clone()
+}
+*/
+
+//use serde_derive::Deserialize;
+
+pub mod index_url;
+
+use index_url::IndexUrl;
+use serde_derive::Deserialize;
+
+use std::sync::OnceLock;
+use std::default::Default;
+
+use crate::utils::get_home_dir;
+
+#[derive(Debug, Clone, Deserialize, Default)]
+pub struct MainConfig {
+    pub index_url: IndexUrl,
+}
+
+
+impl MainConfig {
+    pub fn load_from_path() -> Self {
+        let path = get_home_dir().join("config.toml");
+        if !path.exists() {
+            tracing::event!(tracing::Level::INFO, "Config file not found, using default config.");
+            Self::default().save_to_path();
+        }
+        let config_content = std::fs::read_to_string(path).unwrap();
+        toml::from_str::<Self>(&config_content).unwrap()
+    }
+
+    pub fn save_to_path(&self) {
+        let path = get_home_dir().join("config.toml");
+
+        use toml::to_string_pretty;
+
+        let mut root_table = toml::Table::new();
+        root_table.insert("index_url".to_string(), self.index_url.clone().into());
+        let config_content = to_string_pretty(&root_table).unwrap();
+        
+        std::fs::write(path, config_content).unwrap();
+        
+
+    }
+}
+
+pub static CONFIG: OnceLock<MainConfig> = OnceLock::new();
+
+pub fn get_config() -> MainConfig {
+    CONFIG.get().unwrap().clone()
+}
+
+pub fn init() {
+    let config = &CONFIG;
+    config.set(MainConfig::load_from_path()).unwrap();
 }
--- crates/vcmp_browser_core/src/config/index_url.rs
+++ crates/vcmp_browser_core/src/config/index_url.rs
@@ -1,32 +1,24 @@
-#[derive(Debug, Clone)]
+use serde_derive::Deserialize;
+use std::default::Default;
+
+#[derive(Debug, Clone, Deserialize, Default)]
 pub struct IndexUrl {
+    #[serde(default = "default_master_url")]
     pub master: String,
+    #[serde(default = "default_update_url")]
     pub update: String,
 }
 
-impl Default for IndexUrl {
-    fn default() -> Self {
-        Self {
-            master: "https://txit.top/vcmp".to_string(),
-            update: "https://txit.top/vcmp".to_string(),
-        }
+impl From<IndexUrl> for toml::Value {
+    fn from(url: IndexUrl) -> Self {
+        let mut root_table = toml::Table::new();
+        root_table.insert("master".to_string(), toml::Value::String(url.master));
+        root_table.insert("update".to_string(), toml::Value::String(url.update));
+        
+        toml::Value::Table(root_table)
     }
 }
 
-impl From<toml::Value> for IndexUrl {
-    fn from(value: toml::Table) -> Self {
-        Self {
-            master: value["master"].as_str().unwrap().to_string(),
-            update: value["update"].as_str().unwrap().to_string(),
-        }
-    }
-}
 
-impl Into<toml::Value> for IndexUrl {
-    fn into(self) -> toml::Value {
-        let mut value = toml::value::Table::new();
-        value.insert("master".to_string(), toml::Value::String(self.master));
-        value.insert("update".to_string(), toml::Value::String(self.update));
-        toml::Value::Table(value)
-    }
-}
+fn default_master_url() -> String { "https://txit.top/vcmp".to_string() }
+fn default_update_url() -> String { "https://txit.top/vcmp".to_string() }
