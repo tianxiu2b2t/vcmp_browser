2244be7e5490d78da2b9178168af34ba96276d2e
tianxiu2b2t <atianxiua@163.com>
2025-06-25 00:00:14 +0800

    feat: 增加资源下载和解压以及移到指定的位置

--- crates/vcmp_browser_core/src/resources.rs
+++ crates/vcmp_browser_core/src/resources.rs
@@ -1,7 +1,15 @@
+use std::io::Cursor;
+
 use arc_bytes::ArcBytes;
 use reqwest::Url;
+use tempfile::{tempdir};
 
-use crate::config::get_config;
+use crate::{config::get_config, utils::get_home_dir};
+
+pub struct Resource {
+    pub version: String,
+    pub size: usize
+}
 
 pub fn download_resource<'a>(
     version: &str,
@@ -19,4 +27,52 @@ pub fn download_resource<'a>(
     let response = session.post(uri).multipart(form_data).send().expect("Failed to send request");
     let buffer = response.bytes().expect("Failed to read response");
     Ok(ArcBytes::from(buffer.to_vec()))
+}
+
+/// unpack for 7z
+pub fn unpack_resource(resource: ArcBytes<'_>) -> Result<Resource, String> {
+    let temp_path = tempdir().expect("Failed to create temp directory");
+    let temp_path = temp_path.path();
+    
+    let seek = Cursor::new({
+        let buffer = resource.clone();
+        buffer.to_vec()
+    });
+
+    sevenz_rust::decompress::<Cursor<Vec<u8>>>(seek, temp_path).expect("Failed to unpack resource");
+
+    // read version.txt
+
+    let version_path = std::fs::read_to_string(temp_path.join("version.txt")).expect("Failed to read version.txt");
+    let version = version_path.trim();
+
+    // count files in temp_path
+    let files = std::fs::read_dir(temp_path).expect("Failed to read directory");
+    let files = files.count();
+
+    // move into resources
+    let resource_path = get_home_dir().join("versions").join(version);
+
+    move_files(temp_path, &resource_path).expect("Failed to move files");
+    
+    Ok(Resource {
+        version: version.to_string(),
+        size: files
+    })
+}
+
+fn move_files(src: &std::path::Path, dst: &std::path::Path) -> Result<(), String> {
+    if src.is_file() && dst.is_file() {
+        std::fs::rename(src, dst).expect("Failed to move file");
+    } else if src.is_dir() && dst.is_dir() {
+        let files = std::fs::read_dir(src).expect("Failed to read directory");
+        for file in files {
+            let file = file.expect("Failed to read file");
+            let file_path = file.path();
+            let dst_path = dst.join(file_path.file_name().expect("Failed to get file name"));
+
+            move_files(&file_path, &dst_path).expect("Failed to move file");
+        }
+    }
+    Ok(())
 }
