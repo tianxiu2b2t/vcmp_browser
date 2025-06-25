d21c4d286d1103fc5e579685f5059847632dd2d1
tianxiu2b2t <atianxiua@163.com>
2025-06-25 12:33:56 +0800

    refactor: 重构下载资源

--- crates/vcmp_browser_core/src/resources.rs
+++ crates/vcmp_browser_core/src/resources.rs
@@ -1,16 +1,17 @@
 use std::io::Cursor;
 
-use arc_bytes::ArcBytes;
 use reqwest::Url;
-use tempfile::{tempdir};
 use tracing::{event, Level};
 
-use crate::{config::get_config, utils::{get_home_dir, move_files}};
+use crate::{config::get_config, utils::get_home_dir};
 
+
+#[derive(Clone, Debug)]
 pub struct Resource {
     pub version: String,
     pub size: usize
 }
+/*
 
 pub fn download_resource<'a>(
     version: &str,
@@ -73,3 +74,36 @@ pub fn unpack_resource(resource: ArcBytes<'_>) -> Result<Resource, String> {
     })
 }
 
+*/
+
+pub fn download_resource(
+    version: &str
+) -> Result<Resource, String> {
+    let download_buffer = {
+        let url = &get_config().index_url.update;
+        let uri = Url::parse(({
+            if url.ends_with("/") {
+                format!("{url}download/")
+            } else {
+                format!("{url}/download/")
+            }
+        }).as_str()).expect("Failed to parse URL");
+        let session = reqwest::blocking::Client::new();
+        let form_data = reqwest::blocking::multipart::Form::new().text("json", format!(r#"{{"password": "", "version": "{version}"}}"#));
+        let response = session.post(uri).multipart(form_data).send().expect("Failed to send request");
+        response.bytes().expect("Failed to read response")
+    };
+
+    event!(Level::INFO, "Resource total size: {} bytes", download_buffer.len());
+
+    // unpack for 7z
+    let dest = get_home_dir().join("versions").join(version);
+    event!(Level::INFO, "Unpacking resource to {dest:?}");
+
+    let seek = Cursor::new(download_buffer.clone().to_vec());
+    sevenz_rust::decompress::<Cursor<Vec<u8>>>(seek, dest.clone()).expect("Failed to unpack resource");
+    Ok(Resource {
+        version: version.to_string(),
+        size: std::fs::read_dir(dest.clone()).expect("Failed to read directory").count()
+    })
+}
